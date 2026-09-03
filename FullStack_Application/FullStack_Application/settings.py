f5cc54ccd87ccacc8b44e5ecb3b9547d22736d89
arabaz477 <arabaz477@gmail.com>
2026-09-03 15:53:28 +0530

    Use SQLite

--- FullStack_Application/FullStack_Application/settings.py
+++ FullStack_Application/FullStack_Application/settings.py
@@ -92,18 +92,23 @@ WSGI_APPLICATION = 'FullStack_Application.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE':'django.db.backends.mysql',
+#         'NAME':'FULL_STACK_WEB_RECORD',
+#         'USER':'root',
+#         'PASSWORD':'BAZ477@ALI',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.mysql',
-        'NAME':'FULL_STACK_WEB_RECORD',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
